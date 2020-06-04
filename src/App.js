3cbee0c176306ae137a079dea2705939a81938d0
muhammad-danish1998 <iamdanish1998@gmail.com>
2020-06-05 00:13:17 +0500

    update app.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
          Muhammad Danish :)
+         welcome to the world of CI/CD ..
         </p>
         <a
           className="App-link"
